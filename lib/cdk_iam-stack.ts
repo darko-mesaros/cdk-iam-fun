bcd4127ab8b538b82afcb1c1d9453aa86baf4e8d
darko-mesaros <darko.subotica@gmail.com>
2020-05-04 19:48:47 +0200

    genesis

--- lib/cdk_iam-stack.ts
+++ lib/cdk_iam-stack.ts
@@ -1,9 +1,33 @@
 import * as cdk from '@aws-cdk/core';
+import {PolicyStatementFactory, Action } from 'iam-policy-generator';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as s3 from '@aws-cdk/aws-s3';
+//import * as iam from '@aws-cdk/aws-iam';
+import {Effect} from '@aws-cdk/aws-iam';
 
 export class CdkIamStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // --- bucket ---
+    const myBucket = new s3.Bucket(this, 'myLovelyBucket');
+    
+    // --- lambda ---
+    const myLambda = new lambda.Function(this, 'myAwesomeFunction', {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda')
+    });
+
+    myLambda.addToRolePolicy(
+      new PolicyStatementFactory()
+        .setEffect(Effect.ALLOW)
+        .addResource(myBucket.bucketArn)
+        .addActions([Action.S3.LIST_BUCKET, Action.S3.PUT_OBJECT])
+        .build()
+    );
+    
   }
 }
